f0dbde508027c7546d0f2ea05fd83c7a43b0eae2
svemovr <jovancvetkovic365@gmail.com>
2022-08-05 14:06:44 +0200

    second commit

---
+++ pages/result.py
@@ -0,0 +1,15 @@
+class DuckDuckGoResultPage:
+    def __init__(self,browser):
+        self.browser=browser
+
+    def result_link_titles(self):
+        #TODO
+        return []
+
+    def search_imput_value(self):
+        #TODO
+        return ""
+
+    def title(self):
+        #TODO
+        return ""
---
+++ pages/search.py
@@ -0,0 +1,11 @@
+class DuckDuckGoSearchPage:
+    def __init__(self,browser):
+        self.browser=browser
+
+    def load(self):
+        #TODO
+        pass
+
+    def search(self,phrase):
+        #TODO
+        pass
