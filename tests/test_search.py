fd617258f17f7e957d752413be0bf8f152bee5fb
svemovr <jovancvetkovic365@gmail.com>
2022-08-09 17:38:37 +0200

    another commit chapter 5

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,2 +1,23 @@
+
+
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
+
+
 def test_basic_duckduckgo_search(browser):
-    raise Exception("Incomplite Test")
+    search_page = DuckDuckGoSearchPage(browser)
+    result_page = DuckDuckGoResultPage(browser)
+    PHRASE="panda"
+
+    search_page.load()
+
+    search_page.search(PHRASE)
+
+    assert PHRASE in result_page.title()
+
+    assert PHRASE == result_page.search_imput_value()
+
+    for title in result_page.result_link_titles():
+        assert PHRASE.lower() in title.lower()
+
+    raise Exception("Incomplete Test")
